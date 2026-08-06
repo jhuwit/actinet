9da7e569d158c3374b68184c27821d77725a8efe
muschellij2 <muschellij2@gmail.com>
2026-08-06 12:29:06 -0400

    added specific torch and torchvision for Windows build

--- R/py_require_actinet.R
+++ R/py_require_actinet.R
@@ -5,6 +5,10 @@
 #' @returns A logical value indicating whether the package is available.
 #' @export
 py_require_actinet = function(...) {
-  reticulate::py_require("actinet==0.7.2", python_version = "3.10",
-                         ...)
+  reticulate::py_require(
+    c("actinet==0.7.2",
+      "torch==2.8.0",
+      "torchvision==0.23.0")
+    python_version = "3.10",
+    ...)
 }
